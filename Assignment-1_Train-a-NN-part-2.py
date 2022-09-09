d61a942c31bc2e3825d46bb1bd8be7228330d859
caseyhunt <casey.l.hunt@gmail.com>
2022-09-09 13:12:28 -0600

    Update Assignment-1_Train-a-NN-part-2.py

--- Assignment-1_Train-a-NN-part-2.py
+++ Assignment-1_Train-a-NN-part-2.py
@@ -22,40 +22,71 @@ stdsc = StandardScaler()
 # stdsc.fit(x_train)
 stdsc.fit(x_train_subsplit)
 
-# x_train_std = stdsc.transform(x_train)
+x_train_std = stdsc.transform(x_train)
 x_train_subsplit_std = stdsc.transform(x_train_subsplit)
 x_test_std = stdsc.transform(x_test)
 print(len(x_train_subsplit_std))
 
 
 from sklearn.neural_network import MLPClassifier
-
-n_hidden_nodes = 10
-mlp = MLPClassifier(activation ='tanh', max_iter = 40, random_state = 15, verbose = True, learning_rate_init = 0.01, hidden_layer_sizes=[n_hidden_nodes, n_hidden_nodes])
-mlp.fit(x_train_subsplit_std, y_train_subsplit)
-
-print("Activaton Function: {}".format(mlp.activation))
-print("List of predicted classes: {}".format(mlp.classes_))
-print("Training set loss: {}".format(mlp.loss_))
-
-
 from sklearn import metrics
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-plt.figure(1)
-plt.plot(mlp.loss_curve_)
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
+# n_hidden_nodes = 10
+# mlp = MLPClassifier(activation ='tanh', max_iter = 40, random_state = 15, verbose = True, learning_rate_init = 0.01, hidden_layer_sizes=[n_hidden_nodes, n_hidden_nodes])
+# mlp.fit(x_train_subsplit_std, y_train_subsplit)
 
 
-plt.figure(2)
-y_predicted = mlp.predict(x_test_std)
-print(metrics.classification_report(y_test, y_predicted))
-mat = metrics.confusion_matrix(y_test, y_predicted)
-sns.heatmap(mat.T, square = True, annot = True, fmt = "d", cbar = False)
-plt.xlabel("True Label")
-plt.ylabel("Predicted Label")
+
+start_num_epochs = 10
+finish_num_epochs = 100
+inc_amt = 10
+
+pred_scores = []
+num_epochs = []
+loss_score = []
+
+for epoch_count in range(start_num_epochs, finish_num_epochs, inc_amt):
+  my_classifier = MLPClassifier(activation = 'tanh', random_state = 15, max_iter = epoch_count, learning_rate_init = 0.01, hidden_layer_sizes = [10])
+  my_classifier.fit(x_train_subsplit_std, y_train_subsplit)
+  score = my_classifier.score(x_test_std, y_test)
+  pred_scores.append(score)
+  loss_score.append(my_classifier.loss_)
+  num_epochs.append(epoch_count)
+
+
+fig, ax = plt.subplots()
+ax.plot(num_epochs, pred_scores, 'r-+', linewidth = 2, label = "accuracy")
+ax.plot(num_epochs, loss_score, linewidth =2, label = "loss")
+ax.legend(loc = 'center right')
+ax.set_ylim([0, 1])
+plt.xlabel("Num of epochs")
+plt.ylabel("Accuracy")
+plt.title("Impact of number of training epochs")
+plt.show()
+
+
+# print("Activaton Function: {}".format(mlp.activation))
+# print("List of predicted classes: {}".format(mlp.classes_))
+# print("Training set loss: {}".format(mlp.loss_))
+
+
+
+
+
+# plt.figure(1)
+# plt.plot(mlp.loss_curve_)
+# plt.xlabel("Epochs")
+# plt.ylabel("Loss")
+
+
+# plt.figure(2)
+# y_predicted = mlp.predict(x_test_std)
+# print(metrics.classification_report(y_test, y_predicted))
+# mat = metrics.confusion_matrix(y_test, y_predicted)
+# sns.heatmap(mat.T, square = True, annot = True, fmt = "d", cbar = False)
+# plt.xlabel("True Label")
+# plt.ylabel("Predicted Label")
 
 
