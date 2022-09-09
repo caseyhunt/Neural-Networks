2506ed828f586964348fc84e50b5a081647223ae
caseyhunt <casey.l.hunt@gmail.com>
2022-09-09 12:43:58 -0600

    adding code for question 2

--- Assignment-1_Train-a-NN-part-2.py
+++ Assignment-1_Train-a-NN-part-2.py
@@ -14,21 +14,25 @@ y = wine.target
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 15, test_size=.3)
+x_train_subsplit, x_junk, y_train_subsplit, y_junk = train_test_split(x_train, y_train, random_state = 15, train_size=.2)
 
 
 from sklearn.preprocessing import StandardScaler
 stdsc = StandardScaler()
-stdsc.fit(x_train)
-x_train_std = stdsc.transform(x_train)
-x_test_std = stdsc.transform(x_test)
+# stdsc.fit(x_train)
+stdsc.fit(x_train_subsplit)
 
+# x_train_std = stdsc.transform(x_train)
+x_train_subsplit_std = stdsc.transform(x_train_subsplit)
+x_test_std = stdsc.transform(x_test)
+print(len(x_train_subsplit_std))
 
 
 from sklearn.neural_network import MLPClassifier
 
 n_hidden_nodes = 10
 mlp = MLPClassifier(activation ='tanh', max_iter = 40, random_state = 15, verbose = True, learning_rate_init = 0.01, hidden_layer_sizes=[n_hidden_nodes, n_hidden_nodes])
-mlp.fit(x_train_std, y_train)
+mlp.fit(x_train_subsplit_std, y_train_subsplit)
 
 print("Activaton Function: {}".format(mlp.activation))
 print("List of predicted classes: {}".format(mlp.classes_))
